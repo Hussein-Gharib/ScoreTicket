04afd785c49c500833354e2bc0f7f450244ee48e
Hussein-Gharib <gharibhu56@gmail.com>
2026-06-02 16:40:59 +0200

    Protect admin route on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import ProtectedAdminRoute from "./components/auth/ProtectedAdminRoute";
 import Home from "./pages/Home";
 import Matches from "./pages/Matches";
 import MatchDetails from "./pages/MatchDetails";
@@ -19,7 +20,16 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/my-tickets" element={<MyTickets />} />
-        <Route path="/admin" element={<Admin />} />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedAdminRoute>
+              <Admin />
+            </ProtectedAdminRoute>
+          }
+        />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </MainLayout>
